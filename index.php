5829f287323f0b7012ee99172ccad79d55a66882
antoniomelao1973 <62481493+antoniomelao1973@users.noreply.github.com>
2025-06-27 09:31:51 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ex01_27Jun2025</title>
+</head>
+<body>
+   <h1>Exercício 01 em PHP!!!</h1>
+   <!-- A partir daqui será o código PHP -->
+   <?php
+
+
+    ?>
+</body>
+</html>
